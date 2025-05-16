18bffb4b553bcfc5422c0088df2a58bd9a56f681
s.akhantev <ahantiev@gmail.com>
2025-05-16 12:48:51 +0300

    fix(frontend): App.ts

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import BackendStatus from './components/BackendStatus'
 
@@ -9,15 +7,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <BackendStatus />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
